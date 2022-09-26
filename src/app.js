ba3e3af3d61e50079f00c5637d6938a36b045068
Amir Astaneh <amastaneh@storagevaultcanada.com>
2022-09-26 09:02:17 -0400

    Minor update

--- src/app.js
+++ src/app.js
@@ -1,21 +1,20 @@
 const express = require("express")
-const createError = require("http-errors")
+//const createError = require("http-errors")
 const morgan = require("morgan")
 
-
-// 1- Creating
+// 4- Creating
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// 2- Routing
+// 5- Routing
 app.use('/', require('./routes/index'))
 
-// 3- Catch 404
-app.use(function (_req, res, next) {
-  if (process.env.NODE_ENV === "development") next(createError(404));
-  else res.status(404).send({ message: "Not Found" });
-});
+// 6- Catch 404
+// app.use(function (_req, res, next) {
+//   if (process.env.NODE_ENV === "development") next(createError(404));
+//   else res.status(404).send({ message: "Not Found" });
+// });
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+const http = require('http')
+
 // 1- env file
 require('dotenv').config(
 	{ path: process.env?.NODE_ENV === "development" ? ".env.development" : ".env" }
@@ -10,21 +12,10 @@ app.set('port', port);
 console.log("Environment:", process.env.NODE_ENV);
 console.log("Port:", process.env.PORT, port);
 
-
 // 3- Server creation
-let server
-if (process.env.NODE_ENV === "development") {
-	server = require('http').createServer(app);
-}
-else {
-	const httpsOptions = {
-		key: require('fs').readFileSync('/usr/src/certs/private.key'),
-		cert: require('fs').readFileSync('/usr/src/certs/certificate.crt'),
-		ca: require('fs').readFileSync('/usr/src/certs/ca_bundle.crt'),
-	};
-	server = require('https').createServer(httpsOptions, app);
-}
+const server = http.createServer(app)
 server.listen(port);
+console.log("Server started...");
 
 
 module.exports = server;
